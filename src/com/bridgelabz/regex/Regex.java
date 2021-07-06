989c78fecfec31b07978ef43d151fc775595969f
Atul-Gaikar <84188234+Atul-Gaikar@users.noreply.github.com>
2021-07-06 19:34:38 +0530

    Update Regex.java

--- src/com/bridgelabz/regex/Regex.java
+++ src/com/bridgelabz/regex/Regex.java
@@ -5,61 +5,68 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Regex {
-	public static void main(String[] args) {
+	Scanner SC = new Scanner(System.in);
 
+	boolean First() {
 		String i;
-		Scanner SC = new Scanner(System.in);
 		System.out.println("Enter Your First Name :-");
 		i = SC.next();
-		Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{2}$");
-		Matcher matcher = pattern.matcher(i);
+		Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{2}$");// Regex sintax
+		Matcher matcher = pattern.matcher(i);// Regex sintax
 
-		boolean matchFound = matcher.find();
+		boolean matchFound = matcher.matches();// Use for identification of correct or incorrect.
 		if (matchFound) {
 			System.out.println("According To Pattern Matcher is Correct");
 		} else {
 			System.out.println("According To Pattern Matcher is Inorrect");
 		}
+		return matchFound;
+	}
 
+	boolean Last() {
 		String j;
-		Scanner Scan = new Scanner(System.in);
 		System.out.println("Enter Your Last Name :-");
-		j = Scan.next();
-		Pattern pattern1 = Pattern.compile("^[A-Z]{1}[a-z]{2}$");
-		Matcher matcher1 = pattern1.matcher(j);
-
-//		System.out.println(i);
-		boolean matchFound1 = matcher1.find();
-		if (matchFound1) {
+		j = SC.next();
+		Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{2}$");
+		Matcher matcher = pattern.matcher(j);
+		boolean matchFound = matcher.matches();
+		if (matchFound) {
 			System.out.println("According To Pattern Matcher is Correct");
 		} else {
 			System.out.println("According To Pattern Matcher is Inorrect");
 		}
-		String k;
-		Scanner Scan2 = new Scanner(System.in);
-		System.out.println("Enter Your Email ID :-");
-		k = Scan2.next();
-		Pattern pattern2 = Pattern.compile("^[a-z0-9+_.-]+@[a-zA-Z0-9.-]+$");
-		Matcher matcher2 = pattern2.matcher(k);
+		return matchFound;
+	}
 
-		boolean matchFound2 = matcher2.find();
-		if (matchFound2) {
+	boolean Email() {
+		String k;
+		System.out.println("Enter Your Email ID :-");
+		k = SC.next();
+		Pattern pattern = Pattern.compile("^[a-z0-9+_.-]+@[a-zA-Z0-9.-]+$");
+		Matcher matcher = pattern.matcher(k);
+
+		boolean matchFound = matcher.matches();
+		if (matchFound) {
 			System.out.println("Email address is Correct");
 		} else {
 			System.out.println("Email address is Inorrect");
 		}
-		String l;
-		Scanner Scan3 = new Scanner(System.in);
-		System.out.println("Enter Your Mobile Number :-");
-		l = Scan3.next();
-		Pattern pattern3 = Pattern.compile("^[7-9][0-9]{9}$");
-		Matcher matcher3 = pattern3.matcher(l);
+		return matchFound;
+	}
 
-		boolean matchFound3 = matcher3.find();
-		if (matchFound3) {
+	boolean Mobile() {
+		String l;
+		System.out.println("Enter Your Mobile Number :-");
+		l = SC.next();
+		Pattern pattern = Pattern.compile("^[7-9][0-9]{9}$");
+		Matcher matcher = pattern.matcher(l);
+
+		boolean matchFound = matcher.matches();
+		if (matchFound) {
 			System.out.println("Mobile Number is Correct");
 		} else {
 			System.out.println("Mobile Number is Inorrect");
 		}
+		return matchFound;
 	}
 }
