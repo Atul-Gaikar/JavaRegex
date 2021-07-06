5e5bd6617eef96a510f3fac2db46fb6e0c970bf6
Atul-Gaikar <84188234+Atul-Gaikar@users.noreply.github.com>
2021-07-06 19:28:28 +0530

    Update Regex.java

--- src/com/bridgelabz/regex/Regex.java
+++ src/com/bridgelabz/regex/Regex.java
@@ -1,40 +1,41 @@
 package com.bridgelabz.regex;
 
+
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Regex {
-	public static void main(String[] args) {
+	Scanner SC = new Scanner(System.in);
 
+	boolean First() {
 		String i;
-		Scanner SC = new Scanner(System.in);
 		System.out.println("Enter Your First Name :-");
 		i = SC.next();
-		Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{2}$");
-		Matcher matcher = pattern.matcher(i);
-		
-//		System.out.println(i);
-		boolean matchFound = matcher.find();
+		Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{2}$");// Regex sintax
+		Matcher matcher = pattern.matcher(i);// Regex sintax
+
+		boolean matchFound = matcher.matches();// Use for identification of correct or incorrect.
 		if (matchFound) {
 			System.out.println("According To Pattern Matcher is Correct");
 		} else {
 			System.out.println("According To Pattern Matcher is Inorrect");
 		}
-		
+		return matchFound;
+	}
+
+	boolean Last() {
 		String j;
-		Scanner Scan = new Scanner(System.in);
 		System.out.println("Enter Your Last Name :-");
-		j = Scan.next();
-		Pattern pattern1 = Pattern.compile("^[A-Z]{1}[a-z]{2}$");
-		Matcher matcher1 = pattern1.matcher(j);
-		
-//		System.out.println(i);
-		boolean matchFound1 = matcher1.find();
-		if (matchFound1) {
+		j = SC.next();
+		Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{2}$");
+		Matcher matcher = pattern.matcher(j);
+		boolean matchFound = matcher.matches();
+		if (matchFound) {
 			System.out.println("According To Pattern Matcher is Correct");
 		} else {
 			System.out.println("According To Pattern Matcher is Inorrect");
 		}
+		return matchFound;
 	}
 }
